276cf350a4b431eb293dd67ec5db15108f4b1ab5
JNMYNG <jmj00327@gmail.com>
2023-10-11 20:14:15 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/running_crew_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.running_crew_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:running_crew_app/myApp.dart';
+
+void main() {
+  runApp(const MyApp());
+}
