34596420aef7978d1fea102592b2cc1a11347177
inhatcJ-A-P <guseka22@naver.com>
2023-11-06 19:22:12 +0900

    Update post_detail_screen.dart

--- lib/noticeboard/post_detail_screen.dart
+++ lib/noticeboard/post_detail_screen.dart
@@ -10,11 +10,38 @@ class PostDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(post.title),
+        title: Text(post.title), // Post 제목을 AppBar의 title로 사용
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text(post.content),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 200,
+              child: Image.asset('assets/test1.png'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // 가입하기 버튼이 눌렸을 때의 동작을 여기에 추가하세요.
+              },
+              child: Text("가입하기"),
+            ),
+            SizedBox(height: 20),
+            Text(
+              post.title, // Post 제목을 출력
+              style: TextStyle(fontSize: 50), // 글자 크기를 키움
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                post.content,
+                style: TextStyle(fontSize: 18), // 글자 크기를 조정
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
